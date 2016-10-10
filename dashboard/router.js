c1c27c74b2315cfce1931bc6b47de89a018db982
weapon <weaponhe@126.com>
2016-10-10 10:07:24 +0800

    FIX:login bug

--- dashboard/router.js
+++ dashboard/router.js
@@ -260,15 +260,12 @@ passport.use(new LocalStrategy(
     function (username, password, done) {
         User.findOne({username: username}, function (err, user) {
             if (err) {
-                console.log(1);
                 return done(err);
             }
             if (!user) {
-                console.log(2);
                 return done(null, false, {message: 'Incorrect username.'});
             }
-            if (!user.password === password) {
-                console.log(3);
+            if (!(user.password === password)) {
                 return done(null, false, {message: 'Incorrect password.'});
             }
             return done(null, user);
